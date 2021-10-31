d24ebf8859ab70aeb8edbaa1098eacee864f83c5
Clemens Horn <clemens.horn@mni.thm.de>
2021-10-31 21:07:30 +0100

    tests pass without diff -b option
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- test/test_prog_3.sh
+++ test/test_prog_3.sh
@@ -3,7 +3,7 @@
 echo 1 | ../build/njvm --prog3 > out/prog3.out
 echo 1 | ./njvm --prog3 > out/target.prog3.out
 
-if diff -bc out/prog3.out out/target.prog3.out;
+if diff out/prog3.out out/target.prog3.out;
 then
     echo "PASSED" 
 else
