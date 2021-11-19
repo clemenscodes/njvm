d86451335764975dc3167f995449e1f0470760ea
Clemens Horn <clemens.horn@mni.thm.de>
2021-11-19 04:40:08 +0100

    added test script for prog2.bin
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- test/prog1.sh
+++ test/prog1.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 BUILD="build/njvm"
-BIN="test/prog1.bin"
 BUILD_OUT="test/out/prog1.out"
-$BUILD $BIN > $BUILD_OUT
+
 TEST="test/njvm"
 TEST_OUT="test/out/target.prog1.out"
+
+BIN="test/prog1.bin"
 
 $BUILD $BIN > $BUILD_OUT
 $TEST $BIN > $TEST_OUT
