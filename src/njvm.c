ab9a6d6b8aac4bc09bbc91c644a92a3913e0f036
Clemens Horn <clemens.horn@mni.thm.de>
2022-03-08 15:47:07 +0100

    setting heap and stack size
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/njvm.c
+++ src/njvm.c
@@ -3,7 +3,7 @@
 NinjaVM vm;
 
 int njvm(int argc, char *argv[]) {
-    int i, code_file_specified = 0, debug_mode_activated = 0;
+    int i, code_file_specified = 0, debug_mode_activated = 0, stack_size = 1, heap_size = 1;
     char *code_file = NULL;
     for (i = 1; i <= argc; i++) {
         if (i == argc) {
@@ -23,14 +23,40 @@ int njvm(int argc, char *argv[]) {
         if (!strcmp(argv[i], "--help")) {
             printf("usage: ./njvm [options] <code file>\n");
             printf("Options:\n");
+            printf("  --stack <n> \tset stack size to n KBytes (default: n = 64)\n");
+            printf("  --heap <n> \tset heap size to n KBytes (default: n = 8192)\n");
+            printf("  --gcstats \tshow garbage collection stats\n");
+            printf("  --gcpurge \tpurge old objects after collection\n");
             printf("  --debug \tstart virtual machine in debug mode\n");
             printf("  --version \tshow version and exit\n");
             printf("  --help \tshow this help and exit\n");
             exit(0);
         }
         if (!strcmp(argv[i], "--version")) {
-            printf("Ninja Virtual Machine version %d (compiled Sep 23 2015, 10:36:53)\n", NINJA_BINARY_VERSION);
+            printf("Ninja Virtual Machine version %d (compiled %s, %s)\n", NINJA_BINARY_VERSION, __DATE__, __TIME__);
             exit(0);
+        }
+        if (!strcmp(argv[i], "--stack")) {
+            if (i >= argc - 1) continue;
+            int size = strtol(argv[++i], (char **)NULL, 10);
+            if (stack_size <= 0) continue;
+            stack_size = size;
+            continue;
+        }
+        if (!strcmp(argv[i], "--heap")) {
+            if (i >= argc - 1) continue;
+            int size = strtol(argv[++i], (char **)NULL, 10);
+            if (size <= 0) continue;
+            heap_size = size;
+            continue;
+        }
+        if (!strcmp(argv[i], "--gcstats")) {
+            printf("--gcstats option set\n");
+            continue;
+        }
+        if (!strcmp(argv[i], "--gcpurge")) {
+            printf("--gcpurge option set\n");
+            continue;
         }
         if (!strncmp("-", argv[i], strlen("-"))) {
             printf("unknown command line argument '%s', try '%s --help'\n", argv[i], argv[0]);
