cff2c14726aa14e3aaa89e6d1933f34d64b55ebb
Clemens Horn <clemens.horn@mni.thm.de>
2022-03-10 12:32:31 +0100

    fix mistake, to get forward ptr right shift needs to be done
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- include/macros.h
+++ include/macros.h
@@ -8,7 +8,7 @@
 #define IS_PRIMITIVE(obj_ref) (((obj_ref)->size & MSB) == 0)
 #define GET_ELEMENT_COUNT(obj_ref) ((obj_ref)->size & ~MSB)
 #define GET_REFS_PTR(obj_ref) ((ObjRef *)(obj_ref)->data)
-#define GET_FORWARD_PTR(obj_ref) ((ObjRef *)(obj_ref)->size << 8)
-#define SET_FORWARD_PTR(obj_ref, forward_pointer) ((ObjRef *)(obj_ref)->size = 3 << 62 | forward_pointer)
+#define GET_FORWARD_PTR(obj_ref) ((ObjRef *)(obj_ref)->size >> 8)
+#define SET_FORWARD_PTR(obj_ref, forward_pointer) ((obj_ref)->size = 3 << 62 | forward_pointer)
 
 #endif
