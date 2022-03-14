ce41fb75bcc4f4b6d82e8e3236e31749d40d506c
Clemens Horn <clemens.horn@mni.thm.de>
2022-03-14 19:15:01 +0100

    added get_obj_ref_bytes and size unit tests
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- tests/debugger_test.c
+++ tests/debugger_test.c
@@ -1,6 +1,10 @@
 #include "include/debugger_test.h"
 
 const struct CMUnitTest debugger_unit_tests[] = {
+    cmocka_unit_test(test_print_memory_in_be_bytes),
+    cmocka_unit_test(test_print_memory_in_le_bytes),
+    cmocka_unit_test(test_print_obj_ref_in_be_bytes),
+    cmocka_unit_test(test_print_obj_ref_in_le_bytes),
     cmocka_unit_test(test_debug),
     cmocka_unit_test(test_prompt),
     cmocka_unit_test(test_inspect),
@@ -9,20 +13,7 @@ const struct CMUnitTest debugger_unit_tests[] = {
     cmocka_unit_test(test_run),
     cmocka_unit_test(test_set_breakpoint),
     cmocka_unit_test(test_print_obj_ref),
-    cmocka_unit_test(test_print_memory_in_be_bytes),
-    cmocka_unit_test(test_print_memory_in_le_bytes),
-    cmocka_unit_test(test_print_obj_ref_in_be_bytes),
-    cmocka_unit_test(test_print_obj_ref_in_le_bytes),
 };
-
-void test_debug(void **state) {}
-void test_prompt(void **state) {}
-void test_inspect(void **state) {}
-void test_step(void **state) {}
-void test_next(void **state) {}
-void test_run(void **state) {}
-void test_set_breakpoint(void **state) {}
-void test_print_obj_ref(void **state) {}
 
 void test_print_memory_in_be_bytes(void **state) {
     ObjRef test_obj_ref = new_composite_object(1);
@@ -45,3 +36,12 @@ void test_print_obj_ref_in_le_bytes(void **state) {
     ObjRef test_obj_ref = new_composite_object(1);
     print_obj_ref_in_le_bytes(test_obj_ref);
 }
+
+void test_debug(void **state) {}
+void test_prompt(void **state) {}
+void test_inspect(void **state) {}
+void test_step(void **state) {}
+void test_next(void **state) {}
+void test_run(void **state) {}
+void test_set_breakpoint(void **state) {}
+void test_print_obj_ref(void **state) {}
--- tests/utils_test.c
+++ tests/utils_test.c
@@ -10,21 +10,13 @@ const struct CMUnitTest utils_unit_tests[] = {
     cmocka_unit_test(test_check_ninja_instruction_count),
     cmocka_unit_test(test_check_ninja_variable_count),
     cmocka_unit_test(test_new_composite_object),
-    cmocka_unit_test(test_get_obj_ref_bytes),
-    cmocka_unit_test(test_get_obj_ref_size),
     cmocka_unit_test(test_set_broken_heart),
     cmocka_unit_test(test_set_forward_pointer),
     cmocka_unit_test(test_get_obj_ref_from_forward_pointer),
+    cmocka_unit_test(test_get_obj_ref_bytes),
+    cmocka_unit_test(test_get_obj_ref_size),
 };
 
-void test_read_file(void **state) {}
-void test_read_instructions_into_ir(void **state) {}
-void test_open_file(void **state) {}
-void test_seek_file(void **state) {}
-void test_check_ninja_binary_format(void **state) {}
-void test_close_file(void **state) {}
-void test_check_ninja_instruction_count(void **state) {}
-void test_check_ninja_variable_count(void **state) {}
 void test_new_composite_object(void **state) {
     initialize_heap(DEFAULT_HEAP_SIZE);
     int set_size = 4;
@@ -36,8 +28,30 @@ void test_new_composite_object(void **state) {
         assert_null(GET_REFS_PTR(obj_ref)[i]);
     }
 }
-void test_get_obj_ref_bytes(void **state) {}
-void test_get_obj_ref_size(void **state) {}
+
+void test_get_obj_ref_bytes(void **state) {
+    ObjRef test_obj_ref = new_composite_object(2);
+    unsigned bytes = get_obj_ref_bytes(test_obj_ref);
+    assert_int_equal(bytes, 20);
+    test_obj_ref = new_composite_object(0);
+    bytes = get_obj_ref_bytes(test_obj_ref);
+    assert_int_equal(bytes, 4);
+    test_obj_ref = newPrimObject(8);
+    bytes = get_obj_ref_bytes(test_obj_ref);
+    assert_int_equal(bytes, 12);
+}
+
+void test_get_obj_ref_size(void **state) {
+    ObjRef test_obj_ref = new_composite_object(2);
+    unsigned size = get_obj_ref_size(test_obj_ref);
+    assert_int_equal(size, 2);
+    test_obj_ref = new_composite_object(0);
+    size = get_obj_ref_size(test_obj_ref);
+    assert_int_equal(size, 0);
+    test_obj_ref = newPrimObject(8);
+    size = get_obj_ref_size(test_obj_ref);
+    assert_int_equal(size, 8);
+}
 
 void test_set_broken_heart(void **state) {
     ObjRef test_obj_ref = newPrimObject(6);
@@ -64,7 +78,17 @@ void test_get_obj_ref_from_forward_pointer(void **state) {
     ObjRef test_obj_ref = new_composite_object(2);
     ObjRef new_obj_ref = copy_obj_ref(test_obj_ref);
     ObjRef forward_pointer_decoded_obj_ref = get_obj_ref_from_forward_pointer(test_obj_ref);
+    assert_ptr_not_equal(test_obj_ref, new_obj_ref);
     assert_ptr_equal(forward_pointer_decoded_obj_ref, new_obj_ref);
     print_obj_ref(forward_pointer_decoded_obj_ref);
     print_obj_ref(new_obj_ref);
 }
+
+void test_read_file(void **state) {}
+void test_read_instructions_into_ir(void **state) {}
+void test_open_file(void **state) {}
+void test_seek_file(void **state) {}
+void test_check_ninja_binary_format(void **state) {}
+void test_close_file(void **state) {}
+void test_check_ninja_instruction_count(void **state) {}
+void test_check_ninja_variable_count(void **state) {}
