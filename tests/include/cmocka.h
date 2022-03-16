62de80587a1b28386b9873307c7f79b3c6de4715
Clemens Horn <clemens.horn@mni.thm.de>
2022-03-16 08:21:31 +0100

    update lint in ci
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- tests/include/cmocka.h
+++ tests/include/cmocka.h
@@ -1,9 +1,10 @@
 #ifndef _CMOCKA_H
 #define _CMOCKA_H
 
-#include <cmocka.h>
-#include <setjmp.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
 
 #endif
